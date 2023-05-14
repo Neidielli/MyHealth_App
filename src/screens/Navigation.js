0eca50d541b36c21876d8644a17d6689d6a67d25
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-05-13 22:27:21 -0300

    Atualizando as options do navigation

--- src/screens/Navigation.js
+++ src/screens/Navigation.js
@@ -16,11 +16,11 @@ const Navigation = () => {
             <Stack.Navigator>
                 
                 <Stack.Screen name='Login' component={Login} options={{headerShown:false}} />
-                <Stack.Screen name='Home' component={Home} />
-                <Stack.Screen name='CriarConta' component={CriarConta} />
-                <Stack.Screen name='RecuperarSenha' component={RecuperarSenha} />
-                <Stack.Screen name='NovaVacina' component={NovaVacina} />
-                <Stack.Screen name='EditarVacina' component={EditarVacina} />
+                <Stack.Screen name='Home' component={Home} options={{headerTintColor: '#419ED7',headerTitle: 'Minhas Vacinas', headerStyle: {backgroundColor: '#C1E7F9'}}} />
+                <Stack.Screen name='CriarConta' component={CriarConta} options={{headerTintColor: '#419ED7',headerTitle: 'Nova Conta', headerStyle: {backgroundColor: '#C1E7F9'}}} />
+                <Stack.Screen name='RecuperarSenha' component={RecuperarSenha} options={{headerTintColor: '#419ED7',headerTitle: 'MyHealth', headerStyle: {backgroundColor: '#C1E7F9'}}} />
+                <Stack.Screen name='NovaVacina' component={NovaVacina} options={{headerTintColor: '#419ED7',headerTitle: 'Nova Vacina', headerStyle: {backgroundColor: '#C1E7F9'}}} />
+                <Stack.Screen name='EditarVacina' component={EditarVacina} options={{headerTintColor: '#419ED7',headerTitle: 'Editar Vacina', headerStyle: {backgroundColor: '#C1E7F9'}}} />
         
             </Stack.Navigator>
         </NavigationContainer>
